836f1de010507447bdd6b18b83e94ba2afa6666f
MonalisaPr <40432072+MonalisaPr@users.noreply.github.com>
2018-06-25 16:41:48 +0530

    Update warningalert.component.ts

--- src/app/warningalert/warningalert.component.ts
+++ src/app/warningalert/warningalert.component.ts
@@ -3,11 +3,15 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-warningalert',
   templateUrl: "./warningalert.component.html",
-  styles: [`
-    h2 {
-      color: red;
-    }
-  `]
+  styles: [
+  `
+  p {
+    padding: 20px;
+    background-color: mistyrose;
+    border: 1px solid red;
+  }
+`
+]
 })
 export class WarningAlertComponent {
 
